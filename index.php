893120752d811bae850f53c6b94211c4b08fc39a
VNikkil <48060211+VNikkil@users.noreply.github.com>
2020-08-21 23:16:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <?php
             if(isset($_GET['ID1']) && isset($_GET['ID2']))
             {
+                echo "inside";
                 $Artist1 = new Artist($_GET['ID1']);
                 $Artist2 = new Artist($_GET['ID2']);
 
@@ -23,92 +24,152 @@
                 $Artist_dist_3 = array();
                 $Artist_dist_4 = array();
 
-                $remove = array($_GET['ID1']);
+                $remove = array($Artist1);
 
                 echo "<p> Artist 1 Name :".$Artist1->artistName."</p> <p> Artist 2 Name :".$Artist2->artistName."</p> <br>";
                 $Artist_dist_1 = $Artist1->getRelatedArtists();
 
-                if(in_array ($_GET['ID2'] ,  $Artist_dist_1))     // Checking whether B is in the array 
-                    echo " Path distance = 1";
+                if(in_array ($_GET['ID2'] , $Artist1->RelArtists_IDs))     // Checking whether B is in the array 
+                    {
+                        echo " Path Length = 1";
+                        echo "<br><p>".$Artist1->artistName."</p>";
+                        echo "<p>".$Artist2->artistName."</p>";
+                    }
     
 
                 else{
 
-                    foreach ($Artist_dist_1 as $Temp_ID)
+                    foreach ($Artist_dist_1 as $Temp_Artist)
                     {
                         $Temp_array = array();
-                        $Temp_Artist = new Artist($Temp_ID);
+                        //$Temp_Artist = new Artist($Temp_ID);
                         $Temp_array =  $Temp_Artist->getRelatedArtists();
 
-                        if(in_array ($_GET['ID2'] , $Temp_array))
+                        foreach ($Temp_array as $temporaryartist)
+                        {
+                            $temporaryartist->XartistName = $Temp_Artist->artistName;
+                            $temporaryartist->XartistId = $Temp_Artist->artistId;
+                        }
+                        $Temp_Artist->XartistName = $Artist1->artistName;
+                        $Temp_Artist->XartistId = $Artist1->artistId;
+                        if(in_array ($_GET['ID2'], $Temp_Artist->RelArtists_IDs))
                             {
-                                echo " Path Distance = 2";
+                                
+                                echo " Path Length = 2 ";
+                                
+                                echo "<br><p>".$Artist1->artistName."</p>";
+                               echo "<p>".$Temp_Artist->artistName."</p>"; 
+                               echo "<p>".$Artist2->artistName."</p>";
+
                                 exit();
                             }
 
                         else{
                             $temparray= array();
-                            $temparray = array_diff($Temp_array, $Artist_dist_1,$Artist_dist_2,$remove);     //Adds the artist who are not in  $Artist_dist_1 to  $Artist_dist_2
+                            $temparray = array_udiff($Temp_array, $Artist_dist_1,$Artist_dist_2,$remove,
+                            function($artA,$artB){
+                                if($artA->artistId != $artB->artistId)
+                                return -1;
+
+                                else
+                                return 0;
+                            });     //Adds the artist who are not in  $Artist_dist_1 to  $Artist_dist_2
                             $Artist_dist_2 = array_merge($Artist_dist_2, $temparray); 
                           }    
                     }
+         
+          
                     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-                    foreach($Artist_dist_2 as $Temp_ID)
+                
+                    foreach($Artist_dist_2 as $Temp_Artist)
                     {
                         $Temp_array = array();
 
-                        $Temp_Artist = new Artist($Temp_ID);
                         $Temp_array =  $Temp_Artist->getRelatedArtists();
+                        
+                        foreach ($Temp_array as $temporaryartist)
+                        {
+                            $temporaryartist->XartistName = $Temp_Artist->artistName;
+                            $temporaryartist->XartistId = $Temp_Artist->artistId;
+                        }
 
-                        if(in_array ($_GET['ID2'] , $Temp_array))
+                        if(in_array ($_GET['ID2'] , $Temp_Artist->RelArtists_IDs))
                             {
-                                echo " Path Distance = 3";
-                                exit();
+                                echo " Path Length = 3";
+                                echo "<br><p>".$Artist1->artistName."</p>";
+                                
+                                echo "<p>".$Temp_Artist->artistName."</p>";
+                                echo "<p>".$Temp_Artist->XartistName."</p>";
+                               echo "<p>".$Artist2->artistName."</p>"; 
+                               exit();
                             }
 
                         else{
                             $temparray= array();
-                            $temparray = array_diff($Temp_array, $Artist_dist_1,$Artist_dist_2,$Artist_dist_3,$remove);
+                            $temparray = array_udiff($Temp_array, $Artist_dist_1,$Artist_dist_2,$Artist_dist_3,$remove,
+                            function($artA,$artB){
+                                if($artA->artistId != $artB->artistId)
+                                return -1;
+
+                                else
+                                return 0;
+                            }); 
                             $Artist_dist_3 = array_merge($Artist_dist_3, $temparray);  
                           } 
                     }
 
                     ////////////////////////////////////////////////////////////////////////////////////////////
 
-                    foreach ($Artist_dist_3 as $Temp_ID)
+                    foreach ($Artist_dist_3 as $Temp_Artist)
                     {
                         $Temp_array = array();
 
-                        $Temp_Artist = new Artist($Temp_ID);
                         $Temp_array =  $Temp_Artist->getRelatedArtists();
 
-                        if(in_array ($_GET['ID2'] , $Temp_array))
+                        if(in_array ($_GET['ID2'] , $Temp_Artist->RelArtists_IDs))
                             {
-                                echo " Path Distance = 4";
+                                echo " Path length = 4";
+                                echo "<br><p>".$Artist1->artistName."</p>";
+                                 //SOME CODE NEEDS TO BE ADDED HERE
+                                echo "<p>".$Temp_Artist->artistName."</p>";
+                                echo "<p>".$Temp_Artist->XartistName."</p>";
+                                echo "<p>".$Artist2->artistName."</p>";
+                               
                                 exit();
                             }
 
                          else{
                             $temparray= array();
-                            $temparray = array_diff($Temp_array, $Artist_dist_1,$Artist_dist_2,$Artist_dist_3,$Artist_dist_4,$remove);
+                            $temparray = array_udiff($Temp_array, $Artist_dist_1,$Artist_dist_2,$Artist_dist_3,$Artist_dist_4,$remove, function($artA,$artB){
+                                if($artA->artistId != $artB->artistId)
+                                return -1;
+
+                                else
+                                return 0;
+                            }); 
                             $Artist_dist_4 = array_merge($Artist_dist_4, $temparray);  
                          }   
                     }
 
                     //////////////////////////////////////////////////////////////////////////////////////////////
                     
-                    foreach ($Artist_dist_4 as $Temp_ID)
+                    foreach ($Artist_dist_4 as $Temp_Artist)
                     {
                         $Temp_array = array();
 
-                        $Temp_Artist = new Artist($Temp_ID);
                         $Temp_array =  $Temp_Artist->getRelatedArtists();
 
-                        if(in_array ($_GET['ID2'] , $Temp_array))
+                        if(in_array ($_GET['ID2'] , $Temp_Artist->RelArtists_IDs))
                             {
-                                echo " Path Distance = 5";
+                                echo " Path Length = 5";
+                                echo "<br><p>".$Artist1->artistName."</p>";
+                                //SOME CODE NEEDS TO BE ADDED HERE
+                               echo "<p>".$Temp_Artist->artistName."</p>";
+                               echo "<p>".$Temp_Artist->XartistName."</p>";
+                               echo "<p>".$Artist2->artistName."</p>";
+
+                            
                                 exit();
                             }
                     }
@@ -117,84 +178,6 @@
                 }
             }
              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-                if(!check($_GET['ID1'],$_GET['ID2']))
-                {
-                    $CheckedIds_1[] = $_GET['ID1'];
-                    //$CheckedIds_2[] = $_GET['ID2'];
-          
-                    foreach ($RelArtists_1_id as $Temp_1_ID)
-                    { 
-                        if(! in_array ( $Temp_1_ID, $CheckedIds_1))
-                        {
-                            
-                                foreach( $RelArtists_2_id as $Temp_2_ID)
-                            {
-                                if(! in_array ( $Temp_2_ID, $CheckedIds_2))
-                                 {    
-                                     if(check($Temp_1_ID,$Temp_2_ID))
-                                        {
-                                            echo " Yes They are linked";
-                                            break;
-                                        }
-                                       // $CheckedIds_2[] = $Temp_2_ID;
-                                    }  
-                            }
-
-                            $CheckedIds_1[] = $Temp_1_ID;
-                        }
-                    } 
-                }
-                else
-                echo " Between them 1 person ";
-
-                echo " Out of the IF function";
-               
-            }
-
-            function check($Id1,$Id2)
-            {
-                    $Artist1 = new Artist($Id1);
-                    $Artist2 = new Artist($Id2);
-                    echo "<p> Artist 1 Name :".$Artist1->artistName."</p>  <p> Artist 2 Name :".$Artist2->artistName."</p> <br>";
-                    $RelArtists_1_id = $Artist1->getRelatedArtists();
-                    $RelArtists_2_id = $Artist2->getRelatedArtists();
-
-                /*   for($i =0; $i < count($RelArtists_name) ; $i++){
-                        echo  $RelArtists_name[$i].'<br>';
-                    }
-
-                    $result = (count(array_intersect($RelArtists_1_id, $RelArtists_2_id))) ? true : false;
-
-                    if($result)
-                    echo "true";
-
-                    else
-                    echo "false";
-
-                    return $result;
-
-            }
-*/
         ?>
        
 </body>
